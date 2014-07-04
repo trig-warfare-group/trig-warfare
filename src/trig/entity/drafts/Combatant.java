b88dd5cf7555fd1f3bc2026650b6f0f13859f4fc
MarcosCosmos <m.cosmos42@gmail.com>
2014-07-04 19:46:53 +1000

    rough combatant base class draft
    a rough draft of the combatant class (ie the player or ie, etc)
    
    may be a bit messy and difficult to understand currently, I'm too tired
    to write properly, will fix up tomorrow

---
+++ src/trig/entity/drafts/Combatant.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trig.entity.drafts;
+
+/**
+ * Base class for combatants, such as the player, AI or remote combatants
+ * @author marcos
+ */
+public class Combatant {
+    private int life;
+    private final String name;  
+    //likely to correspond to the Combatant's position in some list?
+    private final int id;
+    
+    //cartesian coordinates
+    private int x;
+    private int y;
+    
+    //the direction in which the combatant is facing. Double unneccassary?
+    //expressed in radians
+    private float direction;
+    
+    //could also have special attributes like speed or armour in the future.
+     
+    public Combatant(int life, String name,int  id){
+        this.life = life;
+        this.name = name;
+        this.id = id;
+        
+        //in the long run there will probably be a list of available spawn points for combatants provided by the map
+        //possibly even segregated into specific types
+        //so this code is temporary
+        this.x = (int) Math.random();
+        this.y = (int) Math.random();
+        this.direction = (float) Math.random();
+    }
+    
+}
+
+/**
+ * Speed and direction in which combatant will move
+ * @author marcos
+ */
+class Trajectory{
+   
+   //Note: using a trajectory instead of just constantly updating current position may be useful for smoothing out lag?
+   //there could be a standard rate of deceleration used so that remote clients can have your combatant moving between updates, possibly?
+   
+   /*there are multiple ways to do this, but since this is only intended for quick use by the map to update positions the following configuration may be ideal:
+        rather than using degrees or radians for the angle, just express it as the ratio between the amount of movement of x and amount of movement of y,
+    
+        such that the new location of the combatant is simply: combatant.x+=speedStuff*angle, combatant.y=speedStuff*angle?
+    
+    
+    */
+   private float angle;
+   
+   
+   //to be used a multiplier rather than a base px per second etc.
+   //float/int not sure?
+   private float speed;
+   
+}
