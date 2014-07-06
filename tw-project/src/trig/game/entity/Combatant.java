aac3911c88dac1e4abf7e735e397f1382381464c
MarcosCosmos <m.cosmos42@gmail.com>
2014-07-06 17:45:24 +1000

    Fleshed out combatant a fair bit
    Note 1: large amount of comments since it's a draft and I don't know how
    familiar everyone is with physics.
    
    Note 2: It occurs to me that remote combatants should be a lot more
    barebones, since they won't necessarily act on their own within your
    client, so I'm going to divy up the classes a bit in my next commit, I
    haven't decided what to call the second class (the one that actually
    contains functions, etc?)

--- tw-project/src/trig/game/entity/Combatant.java
+++ tw-project/src/trig/game/entity/Combatant.java
@@ -7,14 +7,26 @@
 package trig.game.entity;
 
 /**
- * Base class for combatants, such as the player, AI or remote combatants
+ * Base class for combatants, such as the player, AI. NOTE: remote combatants need much less functions etc, perhaps Combatant should be a very barebones class, which only holds data, and use some subclass called Actors to hold the functions?
  * @author marcos
  */
-public class Combatant {
+public class Combatant
+{
+    //constants
+    //is there any reason for these to be public?
+    //a standard amount by which to turn a combatant by at a time? 1/36th of a circle at a time maybe? experimental value
+    private static final float STANDARD_TURN = (float) (((float)1/18)*Math.PI);
+    
+    //a standard amount by which to move at a time? not sure yet if will be expressed in px or something, but yeh. experimental value
+    private static final int STANDARD_DISTANCE = 5;
+    
     private int life;
-    private final String name;  
+    
+    //No need to update name or ID within a single math, so may aswell make them final and publically accessible?
+    
+    public final String name;  
     //likely to correspond to the Combatant's position in some list?
-    private final int id;
+    public final int id;
     
     //cartesian coordinates
     private int x;
@@ -22,11 +34,73 @@ public class Combatant {
     
     //the direction in which the combatant is facing. Double unneccassary?
     //expressed in radians
+    //if memory serves, we will need to use atan2 most often, so the angle will work like atan2 needs it to: 0 is 3 o'clock, must be between [-1pi, 1pi], moves anticlockwise, 12 o'clock is pi/2, 6 o'clock is -pi/2.
     private float direction;
     
+    //movement speed multiplier factor?
+    private float speed;
+    
+    //turn speed multiplier?
+    //Note: possibly not using the agility factor, at least not for turning, we'll see, may be too annoying for mouse users etc.
+    private float agility;
+    
     //could also have special attributes like speed or armour in the future.
-     
-    public Combatant(int life, String name,int  id){
+    
+    /*getters and setter*/
+
+    public int getX()
+    {
+        return x;
+    }
+    
+    //yes we want to be able to publically setX, so the server can forcedly update the position of enemies, possible teleportation, etc
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    }
+    
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    
+    public float getDirection()
+    {
+        return direction;
+    }
+    
+    public void setDirection(float direction)
+    {
+        this.direction = direction;
+    }
+    
+    public float getSpeed()
+    {
+        return speed;
+    }
+    
+    public void setSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+    
+    public float getAgility()
+    {
+        return agility;
+    }
+    
+    public void setAgility(float agility)
+    {
+        this.agility = agility;
+    }
+    
+    public Combatant(int life, String name,int  id)
+    {
         this.life = life;
         this.name = name;
         this.id = id;
@@ -34,18 +108,102 @@ public class Combatant {
         //in the long run there will probably be a list of available spawn points for combatants provided by the map
         //possibly even segregated into specific types
         //so this code is temporary
+        //note: is it worth using a float for x and y? probably not.
         this.x = (int) Math.random();
         this.y = (int) Math.random();
         this.direction = (float) Math.random();
+        
+        this.agility = 1;
+        this.speed = 1;
     }
     
+    /*
+     Some 'helper' functions all of which may not be kept
+     
+    */
+    public boolean isAlive()
+    {
+        return life > 0;
+    }
+    
+    
+    /* movement functions */
+    /**
+     * Rotates the combatant's facing direction.
+     * @param delta number of radians to change the direction by
+     */
+    public void turn(float delta)
+    {
+        
+        float newDirection = direction+delta;
+        //normalise the new direction to be between -pi and pi on every update, so the value never gets to infinity (or out of bounds of the float type/NaN, whatever happens in java), which would reek havoc!
+        while (newDirection <= -Math.PI){
+            newDirection += Math.PI*2;
+        }
+        while (newDirection > Math.PI){
+            newDirection -= Math.PI*2;
+        }
+        direction = newDirection;
+                
+        //maybe push a map update? but I think that the local map would just periodically look
+        //push data up to the server, etc? or just do it periodically? (in server version ofc)
+    }
+    
+    /**
+     * Moves the character along the axis of the combatant's current facing direction
+     * Note: we don't have strafing yet, but we could always add a strafe() function, so it's not a big deal.
+     */
+    public void move(int distance)
+    {
+        //how will we deal with map edges, collisions, etc? should that be handled here and raise the collision event here, or what?
+        
+        //I think this math is correct? Google unit circles and stuff if you wanna know more?
+        int newX = x + (int) Math.round(distance*Math.cos(direction));
+        int newY = y + (int) Math.round(distance*Math.sin(direction));
+        
+        //TODO: DETERMINE IF THERE IS A COLLISION OR AN EDGE OR SOMETHING, ADJUST POSITION ACCORDINGLY AND RAISE A COLLISION EVENT
+        
+        //pseudocode;
+        if(newX > map.width){
+            
+            //we may need to determine or store the coords of the edges of the triangle, or just an algorithm to calculate it once we know shape and size etc.
+            //this wouldn't be too hard to work out how to do, but still, saving it for later.
+            newX = map.width-however much we need to move so that our triangle stays on screen or something? Not just from a straight width or height, what with combatants being trianglular etc.
+        }
+        this.x = newX;
+        this.y = newY;
+    }
+    
+    
+    //QUESTION: Should these "standard" methods be put in the Player object instead of the base one maybe? though it could probably be used in combination with while loops etc by AI, so I'm leaning towards no. 
+    
+    /**
+     * Rotates the combatant's facing direction by a standard amount, multiplied by agility 
+     * @param clockwise boolean used to indicate clockwise-ness of turn?
+     */
+    public void turnStandard(boolean clockwise)
+    { //to be used on keyboard etc events for the player, etc?
+        //clockwise is - for atan2
+        turn( ( clockwise ? -STANDARD_TURN : STANDARD_TURN ) * agility );
+        
+    }
+    
+    /**
+     * Moves the character a by a standard amount, multiplied by speed
+     * @param forward whether the movement is forward or backward
+     */
+    public void moveStandard(boolean forward)
+    {
+        move((int) (( forward ? STANDARD_DISTANCE : -STANDARD_DISTANCE ) * speed));
+    }
 }
 
 /**
- * Speed and direction in which combatant will move
+ * Speed and direction in which combatant will move, possibly for use by the server for anti-lag purposes
  * @author marcos
  */
-class Trajectory{
+class Trajectory
+{
    
    //Note: using a trajectory instead of just constantly updating current position may be useful for smoothing out lag?
    //there could be a standard rate of deceleration used so that remote clients can have your combatant moving between updates, possibly?
