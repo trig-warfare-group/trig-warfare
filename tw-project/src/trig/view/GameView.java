66295d8733bdab44e1ee258efaea633a004bca01
brody <brody.beric@gmail.com>
2014-07-16 21:33:57 +1000

    Merge remote-tracking branch 'remotes/origin/game-mech-0.1.unstable' into game-mech-0.1.unstable
    Conflicts:
    	tw-project/src/trig/game/engine/GameEngine.java
    	tw-project/src/trig/view/GameView.java

--- tw-project/src/trig/view/GameView.java
+++ tw-project/src/trig/view/GameView.java
@@ -3,13 +3,9 @@ package trig.view;
 import trig.game.GameClient;
 import trig.utility.Constants;
 import trig.view.login.LoginView;
-
-<<<<<<< .merge_file_yObR7g
 import java.awt.*;
 
-=======
->>>>>>> .merge_file_zqP3Ti
-public class GameView 
+public class GameView
 {
 	private GameFrame frame;
 	private GamePanel panel;
@@ -17,8 +13,6 @@ public class GameView
 
     public GameView(GameClient gameClient)
     {
-
-<<<<<<< .merge_file_yObR7g
         frame = new GameFrame();
         frame.addModel(gameClient);
         panel = new GamePanel();
@@ -33,34 +27,12 @@ public class GameView
     {
         frame.setTitle(Constants.GAME_TITLE + " - " + Constants.AUTHOR);
         frame.setSize(Constants.WINDOW_DIMENSION);
-        panel.setBackground(new Color(28, 31, 19));
+        panel.setBackground(new Color(7, 22, 0));
         frame.getContentPane().add(panel);
         frame.setResizable(false);
         frame.setVisible(true);
     }
-=======
-		frame = new GameFrame();
-		frame.addModel(gameClient);
-		panel = new GamePanel();
-		panel.addModel(gameClient);
-		this.client = client;
-		this.init();
-	}
-	
-	/**
-	 * Initializes the components to display
-	 */
-	private void init() 
-	{
-		frame.setTitle(Constants.GAME_TITLE + " - " + Constants.AUTHOR);
-		frame.setSize(Constants.WINDOW_DIMENSION);
-		
-		frame.getContentPane().add(panel);
-		frame.setResizable(false);
-		frame.setVisible(true);
-	}
->>>>>>> .merge_file_zqP3Ti
-	
+
 	public GameFrame getGameFrame() 
 	{
 		return frame;
