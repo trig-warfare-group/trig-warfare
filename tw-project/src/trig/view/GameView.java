ea583942aad15e43ca76d695b0e4f73ff2eb4d83
brody <brody.beric@gmail.com>
2014-07-16 16:52:59 +1000

    Merge remote-tracking branch 'remotes/origin/game-mech-0.1.unstable' into game-mech-0.1.unstable
    Conflicts:
    	tw-project/src/trig/game/engine/GameEngine.java
    	tw-project/src/trig/view/GameView.java

--- tw-project/src/trig/view/GameView.java
+++ tw-project/src/trig/view/GameView.java
@@ -4,8 +4,11 @@ import trig.game.GameClient;
 import trig.utility.Constants;
 import trig.view.login.LoginView;
 
+<<<<<<< .merge_file_yObR7g
 import java.awt.*;
 
+=======
+>>>>>>> .merge_file_zqP3Ti
 public class GameView 
 {
 	private GameFrame frame;
@@ -15,6 +18,7 @@ public class GameView
     public GameView(GameClient gameClient)
     {
 
+<<<<<<< .merge_file_yObR7g
         frame = new GameFrame();
         frame.addModel(gameClient);
         panel = new GamePanel();
@@ -34,6 +38,28 @@ public class GameView
         frame.setResizable(false);
         frame.setVisible(true);
     }
+=======
+		frame = new GameFrame();
+		frame.addModel(gameClient);
+		panel = new GamePanel();
+		panel.addModel(gameClient);
+		this.client = client;
+		this.init();
+	}
+	
+	/**
+	 * Initializes the components to display
+	 */
+	private void init() 
+	{
+		frame.setTitle(Constants.GAME_TITLE + " - " + Constants.AUTHOR);
+		frame.setSize(Constants.WINDOW_DIMENSION);
+		
+		frame.getContentPane().add(panel);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
+>>>>>>> .merge_file_zqP3Ti
 	
 	public GameFrame getGameFrame() 
 	{
